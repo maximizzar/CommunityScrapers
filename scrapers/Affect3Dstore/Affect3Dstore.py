675ba882e957faad049cdc8ebfc0cdc353940edc
Maximilian <60427093+maximizzar@users.noreply.github.com>
2024-07-08 03:42:00 +0200

    Update Affect3Dstore.py

--- scrapers/Affect3Dstore/Affect3Dstore.py
+++ scrapers/Affect3Dstore/Affect3Dstore.py
@@ -4,9 +4,10 @@ from typing import List
 import requests
 import sys
 import re
+import base64
 
 from bs4 import BeautifulSoup
-from scrapers.py_common.types import ScrapedStudio, ScrapedScene
+from scrapers.py_common.types import ScrapedStudio, ScrapedScene, ScrapedTag
 
 
 def read_json_Input():
@@ -15,9 +16,14 @@ def read_json_Input():
 
 # scape functions
 def scrape_scene(soup: BeautifulSoup) -> ScrapedScene:
-    title = extract_meta_title(soup)
-    details = extract_details(soup)
-    image = ""
+    return {
+        "title": extract_meta_title(soup),
+        "details": extract_details(soup),
+        "url": url,
+        "image": download_image(soup),
+        "studio": scrape_studio(soup),
+        "tags": scrape_tags(soup),
+    }
 
 
 def scrape_studio(soup: BeautifulSoup) -> ScrapedStudio:
@@ -27,6 +33,10 @@ def scrape_studio(soup: BeautifulSoup) -> ScrapedStudio:
             "name": artist_tag.text.strip(),
             "url": artist_tag.get('href').strip(),
         }
+
+
+def scrape_tags(soup: BeautifulSoup) -> list[ScrapedTag]:
+    return extract_meta_keywords(soup)
 
 
 # extract functions
@@ -41,13 +51,14 @@ def extract_meta_title(soup: BeautifulSoup) -> str | None:
         return None
 
 
-def extract_meta_keywords(soup: BeautifulSoup) -> list[str] | None:
+def extract_meta_keywords(soup: BeautifulSoup) -> list[ScrapedTag] | None:
     # Find the <meta> tag with name="keywords"
     meta_keywords_tag = soup.find('meta', attrs={'name': 'keywords'})
 
     if meta_keywords_tag and 'content' in meta_keywords_tag.attrs:
         keywords_content = meta_keywords_tag['content']
-        keywords_array = [keyword.strip() for keyword in keywords_content.split(',')]
+        keywords_array = [ScrapedTag(name=keyword.strip()) for keyword in keywords_content.split(',')]
+        #keywords_array: list[str] = [keyword.strip() for keyword in keywords_content.split(',')]
         return keywords_array
     else:
         return None
@@ -79,7 +90,7 @@ def extract_description_from_h2(soup: BeautifulSoup) -> str | None:
         return None
 
 
-def extract_image_url(soup: BeautifulSoup) -> list[str] | None:
+def extract_image_url(soup: BeautifulSoup) -> str | None:
     pattern = r'\[data-gallery-role=gallery-placeholder\]'
     script_tags = soup.find_all('script', type='text/x-magento-init')
 
@@ -87,30 +98,34 @@ def extract_image_url(soup: BeautifulSoup) -> list[str] | None:
         match = re.search(pattern, script_tag.text)
         if not match:
             continue
-        array_data = re.search(pattern, script_tag.text)
-        if array_data:
-            array_values = array_data.group(1)
-            array_in_python = eval("[" + array_values + "]")  # Using eval to parse mixed data types
-            return array_in_python
+
+        json_start = script_tag.text.find('{')
+        json_end = script_tag.text.rfind('}') + 1
+        json_object = script_tag.text[json_start:json_end]
+        json_dict = json.loads(json_object)
+        return json_dict.get('[data-gallery-role=gallery-placeholder]').get('mage/gallery/gallery').get('data')[0].get('full')
     return None
 
 
-def save_image_from_url(iurl, save_path):
-    response = requests.get(iurl)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Image saved successfully to {save_path}")
-    else:
-        print("Failed to download the image")
+def download_image(soup: BeautifulSoup) -> str | None:
+    image_url = extract_image_url(soup)
+    image_res = requests.get(image_url)
 
+    if image_res.status_code == 200:
+        # Convert the image content to base64
+        image_base64 = base64.b64encode(image_res.content)
+
+        # Convert bytes to string
+        return image_base64.decode('utf-8')
+    return None
+
+def read_json_input():
+    json_input = sys.stdin.read()
+    return json.loads(json_input)
 
 if __name__ == "__main__":
-    url = "https://affect3dstore.com/corporate-training-2"
-    response = requests.get(url)
-    beautifulSoup = BeautifulSoup(response.text, 'html.parser')
-
-    print(extract_image_url(beautifulSoup))
-
-# data-gallery-role=gallery-placeholder
-    # data json-object
+    if sys.argv[1] == 'scrapeByURL':
+        url = read_json_input().get('url')
+        response = requests.get(url)
+        beautifulSoup = BeautifulSoup(response.text, 'html.parser')
+        print(json.dumps(scrape_scene(beautifulSoup)))
